1c40ca2a18fd6307b22400b1380109a126c91972
Luis-Jimenez-Salgado <ljimenez2024@alu.uct.cl>
2025-04-19 20:37:39 -0400

    Avanze front-end (selección de modulo)

--- ProyectoNuevo/app.py
+++ ProyectoNuevo/app.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 import os
 from tkinter import messagebox
+from PIL import Image
 
 # Configuración de la interfaz
 ctk.set_appearance_mode("dark")
@@ -15,25 +16,35 @@ class ModuleLauncherApp(ctk.CTk):
         self.geometry("400x300")
         self.configure(fg_color="#1c1c1c")
 
+        # Carga imágenes desde la carpeta icons
+        icons_path = os.path.join("ProyectoNuevo", "icons")
+        self.restaurant_icon = ctk.CTkImage(Image.open(os.path.join(icons_path, "restaurant.png")), size=(30, 30))
+        self.disco_icon = ctk.CTkImage(Image.open(os.path.join(icons_path, "disco.png")), size=(30, 30))
+        self.hotel_icon = ctk.CTkImage(Image.open(os.path.join(icons_path, "hotel.png")), size= (30, 30))
+
         # Título
-        self.label_title = ctk.CTkLabel(self, text="Selecciona un módulo", font=("Arial", 24, "bold"))
+        self.label_title = ctk.CTkLabel(self, text="¡Bienvenido! Elige una opción.", font=("Arial", 24, "bold"))
         self.label_title.pack(pady=20)
 
-        # Botones
-        self.create_module_button("Restaurant", "restaurant.py", 1)
-        self.create_module_button("Disco", "disco.py", 2)
-        self.create_module_button("Hotel", "hotel.py", 3)
+        # Botones y sus iconos
+        self.create_module_button("Restaurante", "restaurant.py", self.restaurant_icon)
+        self.create_module_button("Disco", "disco.py", self.disco_icon)
+        self.create_module_button("Hotel", "hotel.py", self.hotel_icon)
 
-    def create_module_button(self, text, module_file, row):
+    def create_module_button(self, text, module_file, icon):
         button = ctk.CTkButton(
             self,
             text=text,
+            image=icon,  # Asigna la imagen al botón
+            compound="left",  # Posición de la imagen respecto al texto
             command=lambda: self.launch_module(module_file),
             font=("Arial", 16, "bold"),
             height=40,
-            fg_color="#3c99dc",
-            hover_color="#4da9eb",
-            corner_radius=10
+            fg_color="#5d3fd3",
+            hover_color="#3f00ff",
+            border_color="#eaddca",
+            border_width=2,
+            corner_radius=43,
         )
         button.pack(pady=10)
 
